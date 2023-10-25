de41d167984c4d1c92a15887d88690166f61abae
Kateryna Zadorozhna <katya.zadorozhna.00@gmail.com>
2023-10-25 19:06:54 +0300

    Add django debug toolbar

--- library_project/settings.py
+++ library_project/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_extensions",
     "books_service.apps.BooksServiceConfig",
+    "debug_toolbar",
     "user",
     "borrowing_service",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -128,3 +130,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- library_project/urls.py
+++ library_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("__debug__/", include("debug_toolbar.urls")),
     path("api/borrowing/", include("borrowing_service.urls", namespace="borrowing")),
 ]
