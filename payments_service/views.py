c28820d82a9175941fa888625bed9dbd6709a787
Denys <deibukdenyscloud@gmail.com>
2023-10-25 22:25:27 +0300

    PAYMENTS: Filter payments based on user and staff status

--- payments_service/views.py
+++ payments_service/views.py
@@ -2,12 +2,23 @@ from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 
 from payments_service.models import Payment
-from payments_service.serializers import PaymentListSerializer, PaymentDetailSerializer
+from payments_service.serializers import (
+    PaymentListSerializer,
+    PaymentDetailSerializer,
+)
 
 
 class PaymentViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentDetailSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(borrowing__user=self.request.user)
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "list":
