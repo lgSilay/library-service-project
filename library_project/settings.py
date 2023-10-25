c13a4f2c9a7c00c3a2b739fb04b9ef15d0b5269f
Ostap Ivakh <o.ivakh95@gmail.com>
2023-10-25 15:32:30 +0300

    Add user/urls.py with endpoints for jwt token & add it to settings & include it in library_project/urls.py

--- library_project/settings.py
+++ library_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework_simplejwt",
     "rest_framework",
     "django_extensions",
     "books_service.apps.BooksServiceConfig",
@@ -127,3 +128,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
