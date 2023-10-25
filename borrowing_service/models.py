b71125aae3622630fa5ee342418906adacda9d22
Inna Isareva <inna8110302@gmail.com>
2023-10-25 16:07:36 +0300

    BORROWING: update borrow_date in model Borrowing

--- borrowing_service/models.py
+++ borrowing_service/models.py
@@ -6,7 +6,7 @@ from books_service.models import Book
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateField()
+    borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey(
