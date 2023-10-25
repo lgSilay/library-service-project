afee0c7cf088786ad54e60d62dca122222443566
Inna Isareva <inna8110302@gmail.com>
2023-10-25 16:49:47 +0300

    BORROWING: added BorrowingCreateSerializer

---
+++ borrowing_service/serializers.py
@@ -0,0 +1,39 @@
+from django.core.exceptions import ValidationError
+from rest_framework import serializers
+
+from borrowing_service.models import Borrowing
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "book",
+            "borrow_date",
+            "expected_return_date"
+        )
+        read_only_fields = ("id", "borrow_date")
+
+    def validate(self, attrs):
+        book = attrs.get("book")
+        if book.inventory < 1:
+            raise serializers.ValidationError(f"Book {book.title} is out of stock")
+
+        expected_return_date = attrs.get("expected_return_date")
+        borrow_date = attrs.get("borrow_date")
+        actual_return_date = attrs.get("actual_return_date")
+        Borrowing.validate_date(
+            expected_return_date,
+            borrow_date,
+            actual_return_date,
+            ValidationError,
+        )
+        return attrs
+
+    def create(self, validated_data):
+        book = validated_data.get("book")
+        borrowing = Borrowing.objects.create(**validated_data)
+        book.inventory -= 1
+        book.save()
+        return borrowing
