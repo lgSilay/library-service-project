2aedc2cbf4b6eb91075f73f62432decdf821bb9e
Ostap Ivakh <o.ivakh95@gmail.com>
2023-10-25 21:24:00 +0300

    Remove unnecessary AuthTokenSerializer and CreateTokenView

--- user/views.py
+++ user/views.py
@@ -1,19 +1,12 @@
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer, AuthTokenSerializer
+from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
-
-
-class CreateTokenView(ObtainAuthToken):
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-    serializer_class = AuthTokenSerializer
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
