e6bd3ec4e56a4f331c810a523392313e0f60d824
Max Katkalov <makskatkalov@gmail.com>
2023-10-25 13:08:49 +0300

    BOOK: Add book model

--- books_service/models.py
+++ books_service/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    COVER_CHOICES = (
+        ("hard", "Hard"),
+        ("soft", "Soft"),
+    )
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        "Author", on_delete=models.CASCADE, related_name="books"
+    )
+    cover = models.CharField(max_length=4, choices=COVER_CHOICES)
+    inventory = models.IntegerField()
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ("title", "author", "cover")
+        ordering = ["title"]
