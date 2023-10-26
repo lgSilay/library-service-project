0d1834edfb01d3e9449130a906a5d2e853a3e900
Max Katkalov <makskatkalov@gmail.com>
2023-10-27 01:58:21 +0300

    ACCESS_TOKEN_LIFETIME changed

--- library_project/settings.py
+++ library_project/settings.py
@@ -125,7 +125,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30000),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
 }
