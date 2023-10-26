dd9136fa578b15a6fb1d114c03533ee3026cf4ed
Max Katkalov <makskatkalov@gmail.com>
2023-10-26 18:15:42 +0300

    Code reformat.

--- user/views.py
+++ user/views.py
@@ -11,7 +11,6 @@ class CreateUserView(generics.CreateAPIView):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get_object(self):
