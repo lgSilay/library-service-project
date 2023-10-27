2dba3292d323ca067309e67239cc1c3018564778
Denys <deibukdenyscloud@gmail.com>
2023-10-27 13:29:42 +0300

    Add sending notifications on borrowings overdue

--- borrowing_service/tasks.py
+++ borrowing_service/tasks.py
@@ -3,7 +3,7 @@ from django.db.models import Q
 from django.utils import timezone
 
 from borrowing_service.models import Borrowing
-from tgbot.notificatior import send_notification
+from tgbot.notificator import send_notification
 
 
 @shared_task
@@ -17,10 +17,10 @@ def check_overdue_borrowings() -> None:
         Q(expected_return_date__lt=timezone.now())
         & Q(actual_return_date__isnull=True)
     )
+    message = "No borrowings overdue today!"
     if overdue_borrowings.exists():
-        for borrowing in overdue_borrowings:
-            chat_id = borrowing.user.telegram_id
-            ...
-    else:
-        ...
-        print("No borrowings overdue today!")
+        borrowings_str = "\n".join(
+            str(borrowing) for borrowing in overdue_borrowings
+        )
+        message = f"Borrowings overdue:\n{borrowings_str}"
+    send_notification(message)
