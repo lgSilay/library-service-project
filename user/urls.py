aa7c1d4756c9c65ef9e2224406a988bdbf19f486
Kateryna Zadorozhna <katya.zadorozhna.00@gmail.com>
2023-10-27 16:27:04 +0300

    Add logging to user

--- user/urls.py
+++ user/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
 
-from user.views import ManageUserView, CreateUserView, TelegramUserView
+from user.views import ManageUserView, CreateUserView, TelegramUserView, CustomTokenObtainPairView
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("register/", CreateUserView.as_view({"post": "create"}), name="create"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-    path("me/", ManageUserView.as_view(), name="manage"),
+    path("me/", ManageUserView.as_view({"put": "update", "get": "retrieve", "patch": "update"}), name="manage"),
     path(
         "telegram/",
         TelegramUserView.as_view(
--- user/views.py
+++ user/views.py
@@ -1,22 +1,54 @@
+import logging
+
 from django.contrib.auth import get_user_model
-from rest_framework import generics, mixins
-from rest_framework.authentication import TokenAuthentication
+from rest_framework import mixins, status
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from user.serializers import UserSerializer, TelegramUserSerializer
 
 
-class CreateUserView(generics.CreateAPIView):
+logger = logging.getLogger("django.user")
+
+
+class CreateUserView(
+    mixins.CreateModelMixin, GenericViewSet
+):
     serializer_class = UserSerializer
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        logger.info("Create user", {"data": serializer.data})
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-class ManageUserView(generics.RetrieveUpdateAPIView):
+
+class ManageUserView(
+    mixins.UpdateModelMixin, mixins.RetrieveModelMixin, GenericViewSet
+):
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, "_prefetched_objects_cache", None):
+            instance._prefetched_objects_cache = {}
+
+        logger.info("Update user", {"data": serializer.data})
+        return Response(serializer.data)
 
 
 class TelegramUserView(
@@ -28,3 +60,29 @@ class TelegramUserView(
 
     def get_object(self):
         return self.request.user
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, "_prefetched_objects_cache", None):
+            instance._prefetched_objects_cache = {}
+
+        logger.info("Update telegram user", {"data": serializer.data})
+        return Response(serializer.data)
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+
+        logger.info("Authenticated user", {"data": serializer.data})
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
