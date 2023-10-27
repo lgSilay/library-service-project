c8b04aedf070fe4d40be39405f72fd1ee9e67939
Denys <deibukdenyscloud@gmail.com>
2023-10-27 12:05:17 +0300

    Fix borrowing and payment service permissions

--- payments_service/views.py
+++ payments_service/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
@@ -15,7 +16,7 @@ from payments_service.permissions import IsOwnerOrAdmin
 class PaymentViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
     queryset = Payment.objects.select_related("borrowing__book")
     serializer_class = PaymentDetailSerializer
-    permission_classes = (IsOwnerOrAdmin,)
+    permission_classes = (IsAuthenticated, IsOwnerOrAdmin)
 
     def get_queryset(self):
         queryset = self.queryset
