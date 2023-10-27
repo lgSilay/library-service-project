56a1c9561a5042edaa507ce70e970bc2a87a595b
Denys <deibukdenyscloud@gmail.com>
2023-10-27 09:32:30 +0300

    PAYMENTS: Add fine payment for book overdue

--- borrowing_service/admin.py
+++ borrowing_service/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from .models import Borrowing
 
-admin.site.register(Borrowing)
+
+@admin.register(Borrowing)
+class BorrowingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "borrow_date",
+        "expected_return_date",
+        "actual_return_date",
+        "book",
+    )
+    search_fields = ("book__title",)
+    sortable_by = ("id",)
--- borrowing_service/urls.py
+++ borrowing_service/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import BorrowingViewSet
+from .views import BorrowingViewSet, ReturnBorrowingView
 
 
 router = routers.DefaultRouter()
@@ -9,6 +9,11 @@ router.register("borrowings", BorrowingViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path(
+        "borrowings/<int:pk>/return/",
+        ReturnBorrowingView.as_view(),
+        name="order_return",
+    ),
 ]
 
 app_name = "borrowing_service"
