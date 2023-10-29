51a8eed6c24cd5f07e7337ce7ad55feafd42422a
Kateryna Zadorozhna <katya.zadorozhna.00@gmail.com>
2023-10-29 23:36:51 +0200

    Set access token lifetime to 30 min

--- library_project/settings.py
+++ library_project/settings.py
@@ -133,7 +133,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30000),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
 }
