f7c3e2dccafa69eea48de0f2d92a9bf96f98f9b0
Kateryna Zadorozhna <katya.zadorozhna.00@gmail.com>
2023-10-30 13:38:33 +0200

    Resolve conflict

--- library_project/urls.py
+++ library_project/urls.py
@@ -10,32 +10,17 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "api/borrowing_service/",
-        include("borrowing_service.urls", namespace="borrowing_service"),
-    ),
+    path("api/borrowing_service/", include("borrowing_service.urls", namespace="borrowing_service")),
     path("api/books/", include("books_service.urls")),
     path("api/user/", include("user.urls", namespace="user")),
-    path(
-        "api/payments_service/",
-        include("payments_service.urls", namespace="payments"),
-    ),
+    path("api/payments/", include("payments_service.urls", namespace="payments")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "api/doc/swagger/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
-    path(
-        "api/doc/redoc/",
-        SpectacularRedocView.as_view(url_name="schema"),
-        name="redoc",
-    ),
+    path("api/doc/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ] + (
     [
         path("__debug__/", include("debug_toolbar.urls")),
-    ]
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     if settings.DEBUG
     else []
 )
